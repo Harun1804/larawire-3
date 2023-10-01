e35e3ca59eacf3f78ed7ec3777d7cc86aeb66ab0
Harun1804 <Harun.arrasyid1804@gmail.com>
2023-10-01 17:15:00 +0700

    Wire navigation

---
+++ resources/views/components/nav-link.blade.php
@@ -0,0 +1,8 @@
+@props(['active' => false])
+@php
+    $clases = ($active ?? false) ? 'nav-link active' : 'nav-link';
+@endphp
+
+<li class="nav-item">
+    <a wire:navigate {{ $attributes->merge(['class' => $clases]) }}>{{ $slot }}</a>
+</li>
