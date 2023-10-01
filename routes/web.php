f0ea189ca18018fa5ab6d12c0b3c623b4d9a8fba
Harun1804 <Harun.arrasyid1804@gmail.com>
2023-10-01 12:46:11 +0700

    Dynamic Title TAb

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,5 @@
 <?php
 
-use App\Livewire\About;
-use App\Livewire\Home;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -15,5 +13,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', Home::class)->name('home');
-Route::get('/about', About::class)->name('about');
+Route::get('/', \App\Livewire\Home::class)->name('home');
+Route::get('/about', \App\Livewire\About::class)->name('about');
+Route::get('/contact', \App\Livewire\Contact::class)->name('contact');
+Route::get('/posts', \App\Livewire\Posts\Index::class)->name('posts.index');
+Route::get('/users/{user}', \App\Livewire\Users\Show::class)->name('users.show');
