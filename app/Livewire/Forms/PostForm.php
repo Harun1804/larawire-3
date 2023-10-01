2bbe6e55158d7cca08e8b5cff6b5ecef75dc5238
Harun1804 <Harun.arrasyid1804@gmail.com>
2023-10-01 19:19:54 +0700

    Livewire form

---
+++ app/Livewire/Forms/PostForm.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Livewire\Forms;
+
+use Livewire\Form;
+use App\Models\User;
+use Livewire\Attributes\Rule;
+
+class PostForm extends Form
+{
+    #[Rule('required')]
+    public string $title = '';
+
+    #[Rule('required')]
+    public string $body = '';
+
+    public function store()
+    {
+        $user = User::find(1);
+        $user->posts()->create(
+            $this->validate()
+        );
+        $this->reset();
+    }
+}
--- app/Livewire/Posts/Create.php
+++ app/Livewire/Posts/Create.php
@@ -2,25 +2,17 @@
 
 namespace App\Livewire\Posts;
 
-use App\Models\User;
 use Livewire\Component;
-use Livewire\Attributes\Rule;
+use App\Livewire\Forms\PostForm;
 
 class Create extends Component
 {
-    #[Rule('required')]
-    public string $title;
 
-    #[Rule('required')]
-    public string $body;
+    public PostForm $form;
 
     public function save()
     {
-        $user = User::find(1);
-        $validate = $this->validate();
-
-        $user->posts()->create($validate);
-        $this->reset();
+        $this->form->store();
     }
 
     public function render()
